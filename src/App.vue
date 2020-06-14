e8571bb32254b3cfcb840acd85e93f7cf632c346
Ismail NGUYEN <Ismail.NGUYEN@loccitane.com>
2020-06-14 12:51:01 +0200

    Init library

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <DarkModeSwitch :switched="onSwitched"/>
+  </div>
+</template>
+
+<script>
+	import DarkModeSwitch from './components/DarkModeSwitch.vue'
+
+	export default {
+		name: 'App',
+		components: {
+			DarkModeSwitch
+		},
+		methods: {
+			onSwitched: function () {
+				console.log('dark mode enabled !')
+			}
+		}
+	}
+</script>
